4d0f281ccc8d9cfd485e873b78819869e69f765c
jaocordeiro <joaocordeiroalmeida01@gmail.com>
2021-08-07 18:41:16 -0300

    stylization screen home

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,11 +1,43 @@
 import React from 'react';
-import {Text} from 'react-native';
-import {Container} from './Home.Styled';
+import {useNavigation} from '@react-navigation/native';
+import {
+  Container,
+  Scroller,
+  HeaderArea,
+  HeaderTitle,
+  SearchButton,
+  LocationArea,
+  LocationInput,
+  LocationFinder,
+} from './Home.Styled';
+import SearchIcon from '../assets/search.svg';
+import MyLocationIcon from '../assets/my_location.svg';
 
 export default () => {
+  const navigation = useNavigation();
+
   return (
     <Container>
-      <Text>Home</Text>
+      <Scroller>
+        <HeaderArea>
+          <HeaderTitle numberOfLines={2}>
+            Encontre seu barbeiro favorito
+          </HeaderTitle>
+          <SearchButton onPress={() => navigation.navigate('Search')}>
+            <SearchIcon width="26" height="26" fill="#fff" />
+          </SearchButton>
+        </HeaderArea>
+
+        <LocationArea>
+          <LocationInput
+            placeholder="Onde voce esta?"
+            placeholderTextColor="#fff"
+          />
+          <LocationFinder>
+            <MyLocationIcon width="26" height="26" fill="#fff" />
+          </LocationFinder>
+        </LocationArea>
+      </Scroller>
     </Container>
   );
 };
