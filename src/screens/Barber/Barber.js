655c538dde66a4712a219786073273dbf3364073
jaocordeiro <joaocordeiroalmeida01@gmail.com>
2021-09-23 15:25:39 -0300

    finish barber screen

--- src/screens/Barber/Barber.js
+++ src/screens/Barber/Barber.js
@@ -18,6 +18,17 @@ import {
   UserFavButton,
   BackButton,
   LoadingIcon,
+  ServicesTitle,
+  ServiceItem,
+  ServiceInfo,
+  ServiceName,
+  ServicePrice,
+  ServiceChooseButton,
+  ServiceChooseBtnText,
+  TestimonialItem,
+  TestimonialInfo,
+  TestimonialName,
+  TestimonialBody,
 } from './Barber.Styled';
 import {useNavigation, useRoute} from '@react-navigation/native';
 import Api from '../../Api';
@@ -25,6 +36,8 @@ import Swiper from 'react-native-swiper';
 import Stars from '../../components/Stars';
 import FavoriteIcon from '../../assets/favorite.svg';
 import BackIcon from '../../assets/back.svg';
+import NavPrevIcon from '../../assets/nav_prev.svg';
+import NavNextIcon from '../../assets/nav_next.svg';
 
 export default () => {
   const navigation = useNavigation();
@@ -93,9 +106,47 @@ export default () => {
             </UserFavButton>
           </UserInfoArea>
           {loading && <LoadingIcon size="large" color="#000000" />}
-          <ServiceArea></ServiceArea>
 
-          <TestimonialArea></TestimonialArea>
+          {userInfo.services && (
+            <ServiceArea>
+              <ServicesTitle>Lista de servicos</ServicesTitle>
+              {userInfo.services.map((item, key) => (
+                <ServiceItem key={key}>
+                  <ServiceInfo>
+                    <ServiceName>{item.name}</ServiceName>
+                    <ServicePrice>R$ {item.price}</ServicePrice>
+                  </ServiceInfo>
+                  <ServiceChooseButton>
+                    <ServiceChooseBtnText>Agendar</ServiceChooseBtnText>
+                  </ServiceChooseButton>
+                </ServiceItem>
+              ))}
+            </ServiceArea>
+          )}
+          {userInfo.testimonials && userInfo.testimonials.length > 0 && (
+            <TestimonialArea>
+              <Swiper
+                style={{height: 110}}
+                showsPagination={false}
+                showsButtons={true}
+                prevButton={
+                  <NavPrevIcon width="35" height="35" fill="#000000" />
+                }
+                nextButton={
+                  <NavNextIcon width="35" height="35" fill="#000000" />
+                }>
+                {userInfo.testimonials.map((item, key) => (
+                  <TestimonialItem key={key}>
+                    <TestimonialInfo>
+                      <TestimonialName>{item.name}</TestimonialName>
+                      <Stars stars={item.rate} showNumber={false} />
+                    </TestimonialInfo>
+                    <TestimonialBody>{item.body}</TestimonialBody>
+                  </TestimonialItem>
+                ))}
+              </Swiper>
+            </TestimonialArea>
+          )}
         </PageBody>
       </Scroller>
       <BackButton onPress={handleBackButton}>
